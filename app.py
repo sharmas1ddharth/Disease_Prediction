5193763f63cc35b13b736e044ca4daeea38df078
Siddharth Sharma <ssiddharth408@gmail.com>
2021-08-31 00:48:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def predict_disease():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(threaded=True, port=5000)
