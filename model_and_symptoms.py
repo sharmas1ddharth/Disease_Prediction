a11de3af636e5bbd0237a562297bd2b63790b927
Siddharth Sharma <ssiddharth408@gmail.com>
2021-08-31 23:24:55 +0530

    Update model_and_symptoms.py

--- model_and_symptoms.py
+++ model_and_symptoms.py
@@ -40,7 +40,7 @@ disease_name = ['Fungal infection', 'Allergy', 'GERD', 'Chronic cholestasis', 'D
                 'Arthritis', '(vertigo) Paroymsal  Positional Vertigo', 'Acne', 'Urinary tract infection', 'Psoriasis',
                 'Impetigo', 'Fungal infection']
 
-model = pickle.load(open("Models\\knn.model", 'rb'))
+model = pickle.load(open("/Models/knn.model", 'rb'))
 
 symptoms_array = []
 for i in symptoms_name:
